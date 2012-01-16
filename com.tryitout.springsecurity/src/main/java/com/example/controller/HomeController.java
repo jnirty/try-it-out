8fbaac328df53eac76b7dea03949c0cd146e1257
Marcin Piczkowski <marcin.piczkowski@gmail.com>
2012-01-16 02:28:02 +0100

    Spring Security 3 application base created (no security yet)

---
+++ com.tryitout.springsecurity/src/main/java/com/example/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.controller;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home(Model model) {
+		model.addAttribute("today", new Date());
+		return "home";	
+	}
+
+}
