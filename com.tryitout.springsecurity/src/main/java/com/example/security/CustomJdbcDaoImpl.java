328c953201681295c025e7c7aa5aec91d61458c5
Marcin Piczkowski <marcin.piczkowski@gmail.com>
2012-02-14 14:33:01 +0100

    added JDBC password change functionality using CustomJdbcDaoImpl, this will be replaced later with Spring implementation

---
+++ com.tryitout.springsecurity/src/main/java/com/example/security/CustomJdbcDaoImpl.java
@@ -0,0 +1,39 @@
+package com.example.security;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+
+/**
+ * This class extends Spring class to add password change functionality, but
+ * sprint already provides a class that gives CRUD operations on users, which
+ * is: o.s.s.privisioning.JdbcUserDetailsManager - this could be used instead.
+ * 
+ */
+public class CustomJdbcDaoImpl extends JdbcDaoImpl implements IChangePassword {
+
+	private Logger logger = Logger.getLogger(CustomJdbcDaoImpl.class);
+
+	@Override
+	public void changePassword(String username, String password) {
+		logger.debug("changePassword - begin - updating user password for user: " + username);
+
+		JdbcTemplate template = getJdbcTemplate();
+		template.update("UPDATE USERS SET PASSWORD =  ? WHERE USERNAME = ?", password, username);
+
+		logger.debug("changePassword - end");
+	}
+
+	/**
+	 * Override superclass method for test purpose only to see that actually
+	 * this service is running
+	 */
+	@Override
+	protected List<GrantedAuthority> loadUserAuthorities(String username) {
+		System.out.println("...loadUserAuthorities...");
+		return super.loadUserAuthorities(username);
+	}
+}
