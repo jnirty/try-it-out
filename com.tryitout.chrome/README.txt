b9b15cf678915f3c832b07aacede63d5cef29b02
Marcin Piczkowski <marcin.piczkowski@gmail.com>
2012-02-19 00:28:13 +0100

    New project for Google Chrome browser extensions examples

---
+++ com.tryitout.chrome/README.txt
@@ -0,0 +1,4 @@
+Begin to write Chrome extensions:
+http://tutorialzine.com/2010/06/making-first-chrome-extension/
+
+http://code.google.com/chrome/extensions/trunk/webRequest.html
---
+++ com.tryitout.chrome/src/main/java/com/tryitout/chrome/ChromeExtensionBuilder.java
@@ -0,0 +1,11 @@
+package com.tryitout.chrome;
+
+/**
+ * Utility class to ZIP extension folder. Creates ZIP archive with .crx
+ * extension ready for installation in Chrome.
+ */
+public class ChromeExtensionBuilder {
+	public static void main(String[] args) {
+		throw new RuntimeException("Not yet implemented");
+	}
+}
