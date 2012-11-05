88f57de0a45097e899ccc55620c802cfdca4493b
Marcin Piczkowski <piczmar@wp.pl>
2012-11-06 00:26:18 +0100

    Replaced testing by console logs with custom assertions

--- com.tryitout.javascripttdd/src/main/webapp/ch1/js/strftime_test.js
+++ com.tryitout.javascripttdd/src/main/webapp/ch1/js/strftime_test.js
@@ -1,6 +1,37 @@
-var date = new Date(2009, 11, 5);
-console.log(date.strftime("%Y"));
-console.log(date.strftime("%m"));
-console.log(date.strftime("%d"));
-console.log(date.strftime("%y"));
-console.log(date.strftime("%F"));
+/**
+ * Assertion function
+ * 
+ * @param message
+ * @param expr
+ * @returns
+ */
+function assert(message, expr) {
+	if (!expr) {
+		throw new Error(message);
+	}
+	assert.count++;
+	return true;
+}
+/**
+ * Outputs errors in HTML element
+ */
+function output(text, color) {
+	var p = document.createElement("p");
+	p.innerHTML = text;
+	p.style.color = color;
+	document.body.appendChild(p);
+}
+
+assert.count = 0;
+
+var date = new Date(2009, 9, 2);
+try {
+	assert("%Y should return full year", date.strftime("%Y") === "2009");
+	assert("%m should return month", date.strftime("%m") === "10");
+	assert("%d should return date", date.strftime("%d") === "02");
+	assert("%y should return year as two digits", date.strftime("%y") === "09");
+	assert("%F should act as %Y-%m-%d", date.strftime("%F") === "2009-10-02");
+	output(assert.count + " tests OK", "#0c0");
+} catch (e) {
+	output("Test failed: " + e.message, "#c00");
+}
