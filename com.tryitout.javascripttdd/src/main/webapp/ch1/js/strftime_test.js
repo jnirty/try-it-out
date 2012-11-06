95da2bbec06bb45629f5d4ddc3b22d841e59a0da
Marcin Piczkowski <piczmar@wp.pl>
2012-11-06 01:01:15 +0100

    Rewriten test script with Test Case

--- com.tryitout.javascripttdd/src/main/webapp/ch1/js/strftime_test.js
+++ com.tryitout.javascripttdd/src/main/webapp/ch1/js/strftime_test.js
@@ -1,3 +1,25 @@
+function testCase(name, tests) {
+	assert.count = 0;
+	var successful = 0;
+	var testCount = 0;
+	for ( var test in tests) {
+		if (!/^test/.test(test)) {
+			continue;
+		}
+		testCount++;
+		try {
+			tests[test]();
+			output(test, "#0c0");
+			successful++;
+		} catch (e) {
+			output(test + " failed: " + e.message, "#c00");
+		}
+	}
+	var color = successful == testCount ? "#0c0" : "#c00";
+	output("<strong>" + testCount + " tests, " + (testCount - successful)
+			+ " failures</strong>", color);
+}
+
 /**
  * Assertion function
  * 
@@ -25,13 +47,22 @@ function output(text, color) {
 assert.count = 0;
 
 var date = new Date(2009, 9, 2);
-try {
-	assert("%Y should return full year", date.strftime("%Y") === "2009");
-	assert("%m should return month", date.strftime("%m") === "10");
-	assert("%d should return date", date.strftime("%d") === "02");
-	assert("%y should return year as two digits", date.strftime("%y") === "09");
-	assert("%F should act as %Y-%m-%d", date.strftime("%F") === "2009-10-02");
-	output(assert.count + " tests OK", "#0c0");
-} catch (e) {
-	output("Test failed: " + e.message, "#c00");
-}
+testCase("strftime test", {
+	"test format specifier %Y" : function() {
+		assert("%Y should return full year", date.strftime("%Y") === "2009");
+	},
+	"test format specifier %m" : function() {
+		assert("%m should return month", date.strftime("%m") === "10");
+	},
+	"test format specifier %d" : function() {
+		assert("%d should return date", date.strftime("%d") === "02");
+	},
+	"test format specifier %y" : function() {
+		assert("%y should return year as two digits",
+				date.strftime("%y") === "09");
+	},
+	"test format shorthand %F" : function() {
+		assert("%F should act as %Y-%m-%d",
+				date.strftime("%F") === "2009-10-02");
+	}
+});
