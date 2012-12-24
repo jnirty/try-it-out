f5395a200817e1b0f7bea6da2cf71031ef38ee82
Marcin Piczkowski <piczmar@wp.pl>
2012-12-24 17:05:00 +0100

    changed authencitation to annotations, moved index.gsp css to separate file, added home controller with security annotations

--- com.tryitout.grails/grails-app/conf/BootStrap.groovy
+++ com.tryitout.grails/grails-app/conf/BootStrap.groovy
@@ -1,22 +1,27 @@
 import com.tryitout.grails.Role
 import com.tryitout.grails.User
 import com.tryitout.grails.UserRole
+
 class BootStrap {
 
-     def init = { servletContext ->
+    def init = { servletContext ->
 
-         def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
-         def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
+        def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
+        def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
 
-         def testUser = new User(username: 'me', enabled: true, password: 'password')
-         testUser.save(flush: true)
+        def testUser = new User(username: 'me', enabled: true, password: 'password')
+        testUser.save(flush: true)
 
-         UserRole.create testUser, adminRole, true
+        def adminUser = new User(username: 'admin', enabled: true, password: 'admin')
+        adminUser.save(flush: true)
 
-         assert User.count() == 1
-         assert Role.count() == 2
-         assert UserRole.count() == 1
-     }
-     def destroy = {
-     }
+        UserRole.create testUser, userRole, true
+        UserRole.create adminUser,adminRole, true
+
+        assert User.count() == 2
+        assert Role.count() == 2
+        assert UserRole.count() == 2
+    }
+    def destroy = {
+    }
 } 
--- com.tryitout.grails/grails-app/controllers/com/tryitout/grails/SecureController.groovy
+++ com.tryitout.grails/grails-app/controllers/com/tryitout/grails/SecureController.groovy
@@ -3,6 +3,7 @@ package com.tryitout.grails
 import grails.plugins.springsecurity.Secured
 
 class SecureController {
+
     @Secured(['ROLE_ADMIN'])
     def index = {
         render 'Secure access only'
